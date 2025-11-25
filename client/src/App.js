cbffc8f6100b4e9b0387f1d33e4f35b4715a67d7
Thrishank <thrishankkuntimaddi@gmail.com>
2025-11-25 18:35:40 +0530

    FEB 1st

--- client/src/App.js
+++ client/src/App.js
@@ -32,107 +32,111 @@ import OAuthSuccess from "./pages/LoginPageComp/OAuthSuccess";
 import RequireAuth from "./components/RequireAuth";
 
 
+import { ToastProvider } from "./contexts/ToastContext";
+
 function App() {
 
   return (
     <AuthProvider>
       <ContactsProvider>
-        <Router>
-          <Routes>
+        <ToastProvider>
+          <Router>
+            <Routes>
 
-            {/* PROTECTED AREA (requires login) */}
+              {/* PROTECTED AREA (requires login) */}
 
-            {/* Home Route */}
-            <Route
-              path="/"
-              element={
-                <RequireAuth>
-                  <MainLayout sidePanel={<HomePanel />}>
-                    <Home />
-                  </MainLayout>
-                </RequireAuth>
-              }
-            />
+              {/* Home Route */}
+              <Route
+                path="/"
+                element={
+                  <RequireAuth>
+                    <MainLayout sidePanel={<HomePanel />}>
+                      <Home />
+                    </MainLayout>
+                  </RequireAuth>
+                }
+              />
 
-            {/* Messages Route */}
-            <Route
-              path="/messages/*"
-              element={
-                <RequireAuth>
-                  <MainLayout sidePanel={<MessagesPanel />}>
-                    <Messages />
-                  </MainLayout>
-                </RequireAuth>
-              }
-            />
+              {/* Messages Route */}
+              <Route
+                path="/messages/*"
+                element={
+                  <RequireAuth>
+                    <MainLayout sidePanel={<MessagesPanel />}>
+                      <Messages />
+                    </MainLayout>
+                  </RequireAuth>
+                }
+              />
 
-            {/* Tasks Route (Placeholder Panel for now) */}
-            <Route
-              path="/tasks"
-              element={
-                <RequireAuth>
-                  <MainLayout sidePanel={<TasksPanel />}>
-                    <MyTasks />
-                  </MainLayout>
-                </RequireAuth>
-              }
-            />
+              {/* Tasks Route (Placeholder Panel for now) */}
+              <Route
+                path="/tasks"
+                element={
+                  <RequireAuth>
+                    <MainLayout sidePanel={<TasksPanel />}>
+                      <MyTasks />
+                    </MainLayout>
+                  </RequireAuth>
+                }
+              />
 
-            {/* Notes Route */}
-            <Route
-              path="/notes"
-              element={
-                <RequireAuth>
-                  <MainLayout sidePanel={<NotesPanel />}>
-                    <Notes />
-                  </MainLayout>
-                </RequireAuth>
-              }
-            />
+              {/* Notes Route */}
+              <Route
+                path="/notes"
+                element={
+                  <RequireAuth>
+                    <MainLayout sidePanel={<NotesPanel />}>
+                      <Notes />
+                    </MainLayout>
+                  </RequireAuth>
+                }
+              />
 
-            {/* Notes Detail Route */}
-            <Route
-              path="/notes/:id"
-              element={
-                <RequireAuth>
-                  <MainLayout sidePanel={<NotesPanel />}>
-                    <Notes />
-                  </MainLayout>
-                </RequireAuth>
-              }
-            />
+              {/* Notes Detail Route */}
+              <Route
+                path="/notes/:id"
+                element={
+                  <RequireAuth>
+                    <MainLayout sidePanel={<NotesPanel />}>
+                      <Notes />
+                    </MainLayout>
+                  </RequireAuth>
+                }
+              />
 
-            {/* Blogs Route (No Sidebar as requested) */}
-            <Route
-              path="/blogs"
-              element={
-                <RequireAuth>
-                  <MainLayout sidePanel={null}>
-                    <Blogs />
-                  </MainLayout>
-                </RequireAuth>
-              }
-            />
+              {/* Blogs Route (No Sidebar as requested) */}
+              <Route
+                path="/blogs"
+                element={
+                  <RequireAuth>
+                    <MainLayout sidePanel={null}>
+                      <Blogs />
+                    </MainLayout>
+                  </RequireAuth>
+                }
+              />
 
-            {/* Workspace Selection (Protected but outside main layout if desired, or inside) */}
-            <Route
-              path="/workspaces"
-              element={
-                <RequireAuth>
-                  <WorkspaceSelect />
-                </RequireAuth>
-              }
-            />
+              {/* Workspace Selection (Protected but outside main layout if desired, or inside) */}
+              <Route
+                path="/workspaces"
+                element={
+                  <RequireAuth>
+                    <WorkspaceSelect />
+                  </RequireAuth>
+                }
+              />
 
-            {/* PUBLIC ROUTES */}
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/verify-email" element={<VerifyEmail />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/reset-password" element={<ResetPassword />} />
-            <Route path="/oauth-success" element={<OAuthSuccess />} />
+              {/* PUBLIC ROUTES */}
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/verify-email" element={<VerifyEmail />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route path="/reset-password" element={<ResetPassword />} />
+              <Route path="/oauth-success" element={<OAuthSuccess />} />
 
-          </Routes>
-        </Router>
+            </Routes>
+          </Router>
+        </ToastProvider>
       </ContactsProvider>
     </AuthProvider>
   );
