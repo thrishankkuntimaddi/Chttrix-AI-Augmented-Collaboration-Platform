6bda7adbf1ed6e0af262f6d82f934d3985e40a6c
Thrishank Kuntimaddi <thrishankkuntimaddi@gmail.com>
2026-01-01 21:25:04 +0530

    cOMPANY

--- client/src/pages/admin/DepartmentManagement.jsx
+++ client/src/pages/admin/DepartmentManagement.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import { Plus, Users, Search, MoreHorizontal, Edit, Trash2, MessageCircle } from 'lucide-react';
 import { useCompany } from '../../contexts/CompanyContext';
