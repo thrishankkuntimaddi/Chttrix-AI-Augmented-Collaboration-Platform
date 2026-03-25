c94a2d549ed74cd40a8666fd7dee6f0fe92e74ad
Thrishank Kuntimaddi <thrishankkuntimaddi@gmail.com>
2026-03-25 12:03:25 +0530

    after restruct

--- client/vite.config.js
+++ client/vite.config.js
@@ -50,6 +50,7 @@ export default defineConfig({
       '@components': path.resolve(__dirname, 'src/components'),
       '@pages': path.resolve(__dirname, 'src/pages'),
       '@services': path.resolve(__dirname, 'src/services'),
+      '@platform': path.resolve(__dirname, '../platform'),
     },
   },
 
